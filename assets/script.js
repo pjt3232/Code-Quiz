adcf9271e592a142af95ba0f2bc9c1bb066965cb
Tranel <tranel@Tranels-MacBook-Air.local>
2023-03-27 13:09:49 -0500

    Fixed HTML, added CSS

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+<div class="question" id="question1">
+                    <h3>Question 1</h3>
+                    <p>Which of the following CSS properties is used to control the space between the content and the border of an element?</p>
+                    <ul>
+                        <li><button>border-spacing</button></li>
+                        <li><button>padding</button></li>
+                        <li><button>margin</button></li>
+                        <li><button>content</button></li>
+                    </ul>
+                </div>
+
+                <div class="question" id="question2">
+                    <h3>Question 2</h3>
+                    <p>Which of the following HTML tags is used to define a list inside an unordered list?</p>
+                    <ul>
+                        <li><button>ul</button></li>
+                        <li><button>ol</button></li>
+                        <li><button>li</button></li>
+                        <li><button>dl</button></li>
+                    </ul>
+                </div>
+
+                <div class="question" id="question3">
+                    <h3>Question 3</h3>
+                    <p>Which of the following JavaScript functions is used to add an element to the beginning of an array?</p>
+                    <ul>
+                        <li><button>unshift()</button></li>
+                        <li><button>push()</button></li>
+                        <li><button>shift()</button></li>
+                        <li><button>pop()</button></li>
+                    </ul>
+                </div>
+
+                <div class="question" id="question4">
+                    <h3>Question 4</h3>
+                    <p>Which method is used to add an element to the end of an array in JavaScript?</p>
+                    <ul>
+                        <li><button>add()</button></li>
+                        <li><button>append()</button></li>
+                        <li><button>push()</button></li>
+                        <li><button>insert()</button></li>
+                    </ul>
+                </div>
+
+                <div class="question" id="question5">
+                    <h3>Question 5</h3>
+                    <p>What is the correct HTML tag for creating a hyperlink?</p>
+                    <ul>
+                        <li><button>link</button></li>
+                        <li><button>a</button></li>
+                        <li><button>href</button></li>
+                        <li><button>img</button></li>
+                    </ul>
+                </div>
