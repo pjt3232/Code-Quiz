44ccfffc6fd706f47f154a427844aaa6478abbae
Tranel <tranel@Tranels-MacBook-Air.local>
2023-03-30 12:53:29 -0500

    Updates and comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//grabs all of the elements needed
 var startButton = document.getElementById("start-game");
 var startScreen = document.getElementById("start-screen");
 var questionContainer = document.getElementById("question-screen");
@@ -11,11 +12,13 @@ var highScoresList = document.getElementById("high-scores-list");
 var endScreen = document.getElementById("end-screen");
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+//starting variables that are needed in multiple functions
 var currentQuestionIndex = 0;
 var timeLeft = 60;
 var score = 0;
 var timerId;
 
+//quiz object that has the question, answer, and choices for each of the five questions
 var quizQuestions = [
     {
         question: "Which of the following CSS properties is used to control the space between the content and the border of an element?",
@@ -44,8 +47,10 @@ var quizQuestions = [
     }
 ];
 
+//listens for the click to start the game
 startButton.addEventListener("click", startQuiz);
 
+//function hides the opening screen and opens the question screen and runs showQuestion() & startTimer()
 function startQuiz() {
     startScreen.classList.add("hide");
     questionContainer.classList.remove("hide");
@@ -53,6 +58,7 @@ function startQuiz() {
     startTimer();
 }
 
+//displays text content as the current quiz question and appends each choice to the document of the current question
 function showQuestion() {
     questionText.textContent = quizQuestions[currentQuestionIndex].question;
     choicesList.innerHTML = ""
@@ -65,6 +71,8 @@ function showQuestion() {
     }
 }
 
+//targets click on any of the choices and checks if the choice is === to the answer. 
+//if  not the answer it deducts 10 seconds off the timer and if it is correct updates score by 10 points
 function checkAnswer(event) {
     var selectedChoice = event.target.textContent;
     var correctChoice = quizQuestions[currentQuestionIndex].answer;
@@ -86,6 +94,7 @@ function checkAnswer(event) {
     }
 }
 
+//sets the time interval of the timer and says to endQuiz() when time is = 0; accounts for 10 second deduction and negative time values
 function startTimer() {
     timerId = setInterval(function() {
         timeLeft--;
@@ -97,12 +106,16 @@ function startTimer() {
     }, 1000);
 }
 
+//ends the quiz and clears time interval, also changes the screen content to new section
 function endQuiz() {
     clearInterval(timerId);
     questionContainer.classList.add("hide");
     endScreen.classList.remove("hide");
 }
 
+//changes screen to new section and makes initials = to input recieved in form
+//an object is created for the high score and then sorts them by highest to lowest while only displaying the top 5 scores 
+// saves all high scores to local storage
 function saveHighScore(event) {
     event.preventDefault();
     endScreen.classList.add("hide");
@@ -122,6 +135,7 @@ function saveHighScore(event) {
     showHighScores();
 }
 
+//displays each high score as a list item on the document
 function showHighScores() {
     highScoresList.innerHTML = "";
     for(var i = 0;i < highScores.length; i++) {
@@ -131,6 +145,7 @@ function showHighScores() {
     }
 }
 
+//listens for click on submit button to save high score to local storage
 submitButton.addEventListener("click", saveHighScore);
 
 
